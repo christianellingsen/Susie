85fb9681952eea5a33f1b53a22f563555dfee101
Christian Ellingsen <ce@circleconsult.dk>
2016-03-31 17:01:17 +0200

    added ThingSpeak lib

--- app/src/main/java/dk/dtu/ce/susie/Main_akt.java
+++ app/src/main/java/dk/dtu/ce/susie/Main_akt.java
@@ -1,5 +1,6 @@
 package dk.dtu.ce.susie;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Main_akt extends AppCompatActivity {
+import com.macroyau.thingspeakandroid.TalkBack;
+import com.macroyau.thingspeakandroid.ThingSpeakChannel;
+import com.macroyau.thingspeakandroid.ThingSpeakService;
+import com.macroyau.thingspeakandroid.model.Channel;
+import com.macroyau.thingspeakandroid.model.ChannelFeed;
+import com.macroyau.thingspeakandroid.model.Feed;
+import com.macroyau.thingspeakandroid.model.PublicChannels;
+import com.macroyau.thingspeakandroid.model.StatusUpdates;
+import com.macroyau.thingspeakandroid.model.TalkBackCommand;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit.Callback;
+
+public class Main_akt extends AppCompatActivity implements View.OnClickListener {
+
+    ThingSpeakChannel tsChannel_write = new ThingSpeakChannel(MyApplication.CHANNEL_ID,MyApplication.WRITE_API_KEY);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Button button = (Button) findViewById(R.id.button);
+    EditText et = (EditText) findViewById(R.id.editText);
+    TalkBack tsTalkBack = new TalkBack(7736,"HD2KH3XWI2Y9T4BB");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_akt);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        et.setOnClickListener(this);
+        button.setOnClickListener(this);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,7 +54,10 @@ public class Main_akt extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +79,20 @@ public class Main_akt extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        if(v== button){
+            int value = 0;
+            value= Integer.parseInt(et.getText().toString());
+            uploadValueToTS(value);
+        }
+        else if (v == et){
+            et.setText("");
+        }
+    }
+
+    private void uploadValueToTS(int value) {
+
     }
 }
---
+++ app/src/main/java/dk/dtu/ce/susie/MyApplication.java
@@ -0,0 +1,16 @@
+package dk.dtu.ce.susie;
+
+import android.app.Application;
+
+/**
+ * Created by ce on 31-03-2016.
+ */
+public class MyApplication extends Application {
+
+    // ThingSpeak parameters
+    public static long CHANNEL_ID = 104531;
+    public static String WRITE_API_KEY = "Z1TWGBJK5QJ0YA0H";
+    public static String READ_API_KEY = "F18H5802GYB9QG6N";
+
+
+}
